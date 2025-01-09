1cac1070cb72275dd83059ae807cdfc082832033
Facundo Robert <robert.facundo@hotmail.com>
2025-01-09 11:15:36 -0300

    ¡

--- src/projects.jsx
+++ src/projects.jsx
@@ -112,7 +112,7 @@ const Projects = () => {
             {
                 src: 'proyectos/cuentos.mp4',
                 desc: 'Mi primera zambullida al universo del HTML y CSS. Margin, padding, display: flex y conocer la maravilla del hover. Aprobado con nota sobresaliente, de yapa aproveché el sitio para subir cuentos que había escrito en otro tiempo.',
-                link: 'https://main--robertfacundo.netlify.app/'
+                link: 'https://robertfacundo.github.io/PreEntrega3Robert/'
             },
             {
                 src: 'proyectos/js.mp4',
